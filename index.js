84444916fd817601729cb1fc61f3c58361265eed
sium <siumhaque1089@gmail.com>
2024-10-01 00:33:36 +0600

    8th commit

--- index.js
+++ index.js
@@ -18,4 +18,6 @@ console.log(elem);
 let elemClass = document.getElementsByClassName('container');
 console.log(elemClass);
 
-elemClass[0].style.background = 'yellow'
+// elemClass[0].style.background = 'yellow'
+elemClass[0].classList.add('bg-primary')
+elemClass[0].classList.add('text-success')
